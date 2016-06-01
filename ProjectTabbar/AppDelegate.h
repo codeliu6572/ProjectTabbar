faee54eb51ff44f642f274ef1456c4fcd6ea748b
codeliu6572 <codeliu6572@163.com>
2016-06-01 14:29:35 +0800

    Initial Commit

---
+++ ProjectTabbar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProjectTabbar
+//
+//  Created by 刘浩浩 on 16/6/1.
+//  Copyright © 2016年 CodingFire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProjectTabbar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectTabbar
+//
+//  Created by 刘浩浩 on 16/6/1.
+//  Copyright © 2016年 CodingFire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
